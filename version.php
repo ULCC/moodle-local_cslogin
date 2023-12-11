c5a3872c81d4ee03cf89fb44f1c3066731b0e7f3
Connor Giles <connor.giles@cosector.com>
2023-12-11 14:45:13 +0000

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_cslogin';
 $plugin->release = '2022092000';
-$plugin->version = 2023112901;
+$plugin->version = 2023121101;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
